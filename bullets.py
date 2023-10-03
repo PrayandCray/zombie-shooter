d830ec6bb56e741dd785d80903022ee2be7eb275
adh <me.ultra.fast@gmail.com>
2023-10-03 10:38:43 -0400

    fixed half of the fucking problem

--- bullets.py
+++ bullets.py
@@ -15,23 +15,22 @@ class Bullet(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
 
     def shoot(self, screen_width, screen_height):
+
         keys = pygame.key.get_pressed()
-
-        mouse_x, mouse_y = pygame.mouse.get_pos()
-        direction = pygame.math.Vector2(mouse_x - self.pos.x, mouse_y - self.pos.y)
-        angle = math.degrees(math.atan2(direction.y, direction.x))
-
-        self.image = pygame.transform.rotate(self.original_image, -angle)
-        self.rect = self.image.get_rect(center=self.pos)
-
         if keys[pygame.K_SPACE]:
-            
+
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            direction = pygame.math.Vector2(mouse_x - self.pos.x, mouse_y - self.pos.y)
+            angle = math.degrees(math.atan2(direction.y, direction.x))
+
+            self.image = pygame.transform.rotate(self.original_image, -angle)
+            self.rect = self.image.get_rect(center=self.pos)
+
             self.x_vel = math.cos(math.radians(angle)) * self.speed
             self.y_vel = math.sin(math.radians(angle)) * self.speed
-            
+
             self.pos.x += self.x_vel
             self.pos.y += self.y_vel
             self.rect.center = self.pos
-
             if self.rect.right < 0 or self.rect.left > screen_width or self.rect.bottom < 0 or self.rect.top > screen_height:
                 self.kill()
